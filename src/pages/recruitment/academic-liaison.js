6eef72fc8fb7f0f9facea119fe63dff47b948e70
Qingyi <xqy1115@gmail.com>
2020-09-20 01:32:51 +0800

    Acade Page

--- src/pages/recruitment/academic-liaison.js
+++ src/pages/recruitment/academic-liaison.js
@@ -10,26 +10,26 @@ const AcademicLiaison = () => {
     team: {
       people: [
         {
-          name: 'Gu Geng',
+          name: 'Mengxi',
           position: 'Director',
         },
         {
-          name: 'Ivan Koh',
+          name: 'Shimin',
           position: 'Deputy Director',
         },
       ],
       query: useStaticQuery(graphql`
         query {
-          GuGeng: file(relativePath: { eq: "gugeng.jpg" }) {
+          Mengxi: file(relativePath: { eq: "mengxi.jpg" }) {
             childImageSharp {
-              fluid(maxWidth: 400, maxHeight: 250) {
+              fluid(maxWidth: 400, maxHeight: 500) {
                 ...GatsbyImageSharpFluid
               }
             }
           }
-          IvanKoh: file(relativePath: { eq: "ivankoh.jpg" }) {
+          Shimin: file(relativePath: { eq: "shimin.jpg" }) {
             childImageSharp {
-              fluid(maxWidth: 400, maxHeight: 250) {
+              fluid(maxWidth: 400, maxHeight: 400) {
                 ...GatsbyImageSharpFluid
               }
             }
@@ -40,81 +40,22 @@ const AcademicLiaison = () => {
     sections: [
       {
         title: 'Overview',
-        content: `The Academic Liaison cell aims to promote the academic interests and facilitate the academic pursuits of the student body in the School of Computing (SoC). The cell seeks to be the bridge between the student population and the school with regards to academic matters. The cell initiates activities such as Mock Practical Examinations, Academic Day and Hackathons to ensure that students of SoC have enough resources to excel.        `,
+        content: 'The Academic Liaison cell aims to promote the academic interests and facilitate the academic pursuits of the student body in the School of Computing (SoC). The cell seeks to be the bridge between the student population and the school with regards to academic matters. The cell initiates activities such as Mock Practical Examinations, Academic Day and Hackathons to ensure that students of SoC have enough resources to excel.        ',
+      },
+      {
+        divider: true,
       },
       {
         title: 'Vision',
-        content: 'Leave no one in SOC behind',
-      },
-      {
-        title: 'Mission',
-        content: 'To spread FUN and BUILD relationships in the SOC community through various events.',
-      },
-      {
-        divider: true,
-      },
-      {
-        title: 'Who we are looking for',
         bullets: [
-          'If you are someone with event-planning experiences,',
-          'or someone with brilliant ideas to share and fun initiates that you want to implement for the rest of SOC students,',
-          'or simply someone with passion to connect and to rejuvenate the community',
+          'If you are someone with event-planning experiences,To form an open community and a strong network of support for all Computing students',
+          'To be a robust and encouraging platform for students to explore and experiment',
+          'To instil a deep sense of life-long learning amongst students',
+          'To prepare the student body to face the problems of tomorrow',
         ],
+        content: 'We’d love for you to be a part of the Academic Liaison cell as Project Directors, Vice Project Directors, committee members and helpers. Scroll down to find out more about different roles within the Academic Liaison family! ',
       },
-      {
-        
-        content: 'WAIT NO MORE AND JOIN US!',
-      },
-      {
-        title: 'Events',
-        bullets: [
-          'Computing Day - a SPECIAL day for ALL OF US, only the students of SOC!',
-          'Meliorium - amazing and fun activities for you and your friends to make even MORE friends! (e.g. Amazing Race & Escape Room)',
-          'X men - Facilitate small interest groups formation and provide funding for offline bondings (WOW!)',
-          'Initiate your own new event! - And much more fun that you WISH to see and bring to the SOC!',
-        ],
-      },
-      {
-        divider: true,
-      },
-      {
-        title: 'Responsibilities',
-        subsections: [
-          {
-            title: 'Project Director',
-            bullets: [
-              'to spearhead and oversee the planning for a major event',
-              'coordinate work among the rest of project members',
-            ],
-          },
-          {
-            title: 'Vice Project Director',
-            bullets: [
-              'to assist project director in the planning of a major event',
-              'be in charge of admin/manpower/finance/logistics aspects of the event',
-            ],
-          },
-          {
-            title: 'Project Manager',
-            bullets: [
-              'to be involved in some aspects of the event planning process',
-              'assist in the management of admin/manpower/finance/logistics aspects of the event',
-            ],
-          },
-          {
-            title: 'Onsite Manager',
-            bullets: [
-              'to be involved in some aspects of the event planning process',
-              'to ensure the smooth running of the actual events and make sure changes/needs are immediately attended to',
-            ],
-            subsections: [
-              {
-                content: 'P.S. dont be afraid to take up more than one role or get involved in more than one event!!',
-              },
-            ],
-          },
-        ],
-      },
+
     ],
   }
 
