e6362b91e292f270da8b0c6c47ed0f03bd2b551b
Jeff Sieu <jeffsy00@gmail.com>
2020-09-20 22:15:55 +0800

    Fix sign up button

--- src/components/sign-up-button.js
+++ src/components/sign-up-button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 
-const SignUpButton = () => <Button variant='contained' color='secondary' to='http://bit.ly/23MCSubcommTele'>Join us now</Button>;
+const SignUpButton = () => <Button variant='contained' color='secondary' href='http://bit.ly/23MCSubcommTele'>Join us now</Button>;
 
 export default SignUpButton
