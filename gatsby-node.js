49ab503e51e7899ef51a4119d8a4251bbc30c07b
Jeff Sieu <jeffsy00@gmail.com>
2020-09-20 19:12:53 +0800

    Rename build folder to docs/

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ exports.onCreateDevServer = ({ app }) => {
 
 exports.onPreInit = () => {
   if (process.argv[2] === "build") {
-    fs.rmdirSync(path.join(__dirname, "dist"), { recursive: true })
+    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
     fs.renameSync(
       path.join(__dirname, "public"),
       path.join(__dirname, "public_dev")
@@ -17,7 +17,7 @@ exports.onPreInit = () => {
 }
 
 exports.onPostBuild = () => {
-  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "dist"))
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
   fs.renameSync(
     path.join(__dirname, "public_dev"),
     path.join(__dirname, "public")
